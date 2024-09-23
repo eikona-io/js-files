9b341a56b5227eeb0c4ac7b81f376ae0038407ab
omer <omer@eikona.io>
2024-09-23 17:55:15 +0300

    loadExperiemnts: createBanner: override font size

--- loadExperiments.js
+++ loadExperiments.js
@@ -338,7 +338,7 @@ export function createBanner(divElement, options = {}) {
   textElement.innerText = text;
   Object.assign(textElement.style, {
     color: textColor,
-    fontSize: '6vh',
+    fontSize: '2.5vh',
     fontWeight: 'bold',
     textShadow: textShadow,
     pointerEvents: 'none',
@@ -354,7 +354,7 @@ export function createBanner(divElement, options = {}) {
   subTextElement.innerText = subText;
   Object.assign(subTextElement.style, {
     color: subTextColor,
-    fontSize: '2.5vh',
+    fontSize: '1.5vh',
     fontWeight: 'normal',
     textShadow: subTextShadow,
     pointerEvents: 'none',
