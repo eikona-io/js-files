12b3a9e29c440f04bafc3d8f51ac28f9d474cca0
omer <omer@eikona.io>
2024-09-23 10:38:54 +0300

    createBanner: finish styling

--- createBanner.js
+++ createBanner.js
@@ -2,11 +2,14 @@ function createBanner(divElement, options = {}) {
   const {
     shape = 'right-diagonal',
     text = '',
+    subText = '',
     textColor = '#ffffff',
+    subTextColor = '#ffffff',
     shapeColor = '#000000',
-    textSize = '4rem',
-    textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)',
-    diagonalAngle = 33.33
+    textSize = '4.5rem',
+    subTextSize = '2rem',
+    textShadow = '-10px 10px 20px rgba(0, 0, 0, 0.7)',
+    subTextShadow = '-5px 5px 10px rgba(0, 0, 0, 0.7)',
   } = options;
 
   divElement.style.position = 'relative';
@@ -22,12 +25,15 @@ function createBanner(divElement, options = {}) {
   shapeOverlay.style.display = 'flex';
   shapeOverlay.style.alignItems = 'center';
   shapeOverlay.style.justifyContent = 'center';
+  shapeOverlay.style.fontFamily = 'Gotham, sans-serif';
+  shapeOverlay.style.fontWeight = 'bold';
+
 
   let clipPath;
   let textContainerStyle = {};
   switch (shape) {
     case 'right-diagonal':
-      clipPath = `polygon(75% 0%, 100% 0%, 100% 100%, 55% 100%)`;
+      clipPath = `polygon(75% 0%, 100% 0%, 100% 100%,60% 100%)`;
       textContainerStyle = { right: '0', width: '30%', height: '100%' };
       break;
     case 'left-diagonal':
@@ -51,6 +57,7 @@ function createBanner(divElement, options = {}) {
   Object.assign(textContainer.style, {
     position: 'absolute',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'hidden',
@@ -71,7 +78,23 @@ function createBanner(divElement, options = {}) {
     overflowWrap: 'break-word'
   });
 
+  const subTextElement = document.createElement('div');
+  subTextElement.innerText = subText;
+  Object.assign(subTextElement.style, {
+    color: subTextColor,
+    fontSize: subTextSize,
+    fontWeight: 'normal',
+    textShadow: subTextShadow,
+    pointerEvents: 'none',
+    textAlign: 'center',
+    width: '100%',
+    wordWrap: 'break-word',
+    overflowWrap: 'break-word',
+    marginTop: '1rem'
+  });
+
   textContainer.appendChild(textElement);
+  textContainer.appendChild(subTextElement);
   shapeOverlay.appendChild(textContainer);
   divElement.appendChild(shapeOverlay);
 }
