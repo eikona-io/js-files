053aa95462416b4208bb386a7d17b9cb88b89997
omer <omer@eikona.io>
2024-11-19 19:42:08 +0200

    loadExperiments: bootstrap bugfix

--- loadExperiments.js
+++ loadExperiments.js
@@ -207,7 +207,14 @@ async function initializeAndLoadExperiments(customerId, enableLogging = false) {
   // Only initialize PostHog if it hasn't been initialized yet
   if (!window.posthog.__loaded) {
     const experimentsVariants = evaluateExperimentVariants(experimentsConfigs);
-    posthog.init(posthogToken, { api_host: posthogHost, person_profiles: 'always', enable_heatmaps: true, bootstrap: { featureFlags: experimentsVariants } });
+    posthog.init(posthogToken, {
+      api_host: posthogHost,
+      person_profiles: 'always',
+      enable_heatmaps: true,
+      bootstrap: {
+        featureFlags: experimentsVariants,
+      },
+    });
   }
   // Initialize Sanity
   initializeSanity(sanityProjectId, dataset);
