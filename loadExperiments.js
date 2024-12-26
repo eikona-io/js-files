fd466231d5b6b6b25def3802499942eab8e0d958
omer <omer@eikona.io>
2024-12-26 15:04:25 +0200

    loadExpdriments: set property with lock function

--- loadExperiments.js
+++ loadExperiments.js
@@ -389,6 +389,7 @@ function handleImgTag(element, asset, elementSize, isMobileAsset, imageUrl) {
   }
   logger('elementttt src', element.src);
   lockElementProperty(element, 'src', imageUrl);
+  logger('e src', element);
   element.srcset = "";
   element.style.objectFit = 'cover';
   const sourceElement = element.parentElement.querySelector('source');
